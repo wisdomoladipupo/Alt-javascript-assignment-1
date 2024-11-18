b21c94b3df940442337e2d3af166dd547cf0ba1b
wisdom oladipupo <wisbel71@gmail.com>
2024-11-18 20:42:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const name = "wisdom"
+
+const Age = "2 years"
+
+
+
+
+let isMarried = true : false 
+
+
+
+if(isMarried){
+    alert("is married to zainab cuz she's my only bea")    
+}
+
+
+if(isMarried){
+
+}
+
+function confirmName() {
+confirm("is your name wisdom")
+}
+
+confirmName()
+
+
+
+
+function showname(){
+    alert(`my name is ${name}`)
+}
+
+showname()
+
+
+console.log(name)
+console.log(Age)
+console.log(isMarried)
